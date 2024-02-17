02e7456a1a4f6092dfea33f676c2575a4062baf0
Moinul Islam <moinulilm10@gmail.com>
2024-02-17 12:48:39 +0600

    data added for project

---
+++ src/data/DropDownData.js
@@ -0,0 +1,95 @@
+export const dropdownData = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Products",
+    icon: true,
+    submenu: [
+      {
+        title: "Product 1",
+        path: "/products/Daily",
+      },
+      {
+        title: "Product 2",
+        path: "/products/Luxury",
+      },
+      {
+        title: "Product 3",
+        path: "/products/Comfortable",
+      },
+    ],
+  },
+  {
+    title: "Categories",
+    icon: true,
+    submenu: [
+      {
+        title: "Fashion Collection",
+        submenu: [
+          {
+            title: "Mens Clothing Collections",
+            submenu: [
+              {
+                title: "T-Shirts",
+                path: "/categories/fashion-collection/mens-clothing-collections/t-shirts",
+              },
+              {
+                title: "Pants",
+                path: "/categories/fashion-collection/mens-clothing-collections/pants",
+              },
+            ],
+          },
+          {
+            title: "Women Clothing Collections",
+            submenu: [
+              {
+                title: "Three Piece Collection",
+                path: "/categories/fashion collection",
+              },
+              {
+                title: "Saree Collection",
+                path: "/categories/fashion collection",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Pages",
+    icon: true,
+    submenu: [
+      {
+        title: "Page 1",
+        path: "/pages/Newsletter",
+      },
+    ],
+  },
+  {
+    title: "Campaign",
+    path: "/campaign",
+  },
+  {
+    title: "Offer",
+    path: "/offer",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Review",
+    path: "/review",
+  },
+  {
+    title: "Flash Sale",
+    path: "/flash-sale",
+  },
+  {
+    title: "About us",
+    path: "/about-us",
+  },
+];
---
+++ src/data/OfferData.js
@@ -0,0 +1,40 @@
+import { BsBag } from "react-icons/bs";
+
+export const offersData = [
+  {
+    title: "Gadget Store",
+    offer: "30% Sale",
+    buttonText: "Buy Now",
+    icon: <BsBag />,
+    image: "../assets/airpod.png",
+    backgroundColor: "#EFF9FF",
+    borderColor: "#E2F3FF",
+  },
+  {
+    title: "Bundle Package",
+    offer: "Save 30%",
+    buttonText: "See All",
+    icon: null,
+    image: "../assets/kitkat.png",
+    backgroundColor: "#FFF8F0",
+    borderColor: "#FDF1E5",
+  },
+  {
+    title: "Valentines Offer",
+    offer: "30% Sale",
+    buttonText: "Buy Now",
+    icon: <BsBag />,
+    image: "../assets/ring.png",
+    backgroundColor: "#EFF9FF",
+    borderColor: "#E2F3FF",
+  },
+  {
+    title: "Relax Chair",
+    offer: "New Arrival",
+    buttonText: "Buy Now",
+    icon: <BsBag />,
+    image: "../assets/chair.png",
+    backgroundColor: "#FFECF0",
+    borderColor: "#FFE1E7",
+  },
+];
