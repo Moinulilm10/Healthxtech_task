b0f16f31eaca3d34c91e3f0a656bfdb0d4975065
Moinul Islam <moinulilm10@gmail.com>
2024-02-18 23:25:07 +0600

    loading spinner updated

--- src/components/Products/ProductLoading.jsx
+++ src/components/Products/ProductLoading.jsx
@@ -2,8 +2,22 @@ import "./ProductLoading.css";
 
 const ProductLoading = () => {
   return (
-    <div className="loader-container">
-      <div className="product-loader" />
+    // <div className="loader-container">
+    //   <div className="product-loader" />
+    // </div>
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
